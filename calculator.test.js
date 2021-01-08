fc8e3fe06bf2c6459d13fbd2142c416d5f3881fb
Seba Soto <sebastian.pkfd@gmail.com>
2021-01-07 22:41:03 -0300

    Completed calculator tests

--- calculator.test.js
+++ calculator.test.js
@@ -1,9 +1,21 @@
 import { add, sub, mult, div } from './calculator';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
+test(' 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toEqual(3);
 });
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sub(2, 2)).toBe(0);
-  });
+test('2 - 2 to equal 0', () => {
+    expect(sub(2, 2)).toEqual(0);
+});
+
+test('3 * 5 to equal 15', () => {
+    expect(mult(3, 5)).toEqual(15);
+})
+
+test('10 / 5 to equal 2', () => {
+    expect(div(10, 5)).toEqual(2)
+})
+
+test('10 / 0 to be Null', () => {
+    expect(div(10, 0)).toBeNull();
+})
