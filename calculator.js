d3f93f4ed3edd54c1ef488411e80264f9c60932a
Seba Soto <sebastian.pkfd@gmail.com>
2021-01-07 22:35:51 -0300

    Started calculator tests

---
+++ calculator.js
@@ -0,0 +1,11 @@
+const add = (a, b) => a + b;
+
+const sub = (a, b) => a - b;
+
+const mult = (a, b) => a * b;
+
+const div = (a, b) => {
+    return (b === 0) ? null: a/b;
+}
+
+export { add, sub, mult, div}
---
+++ calculator.test.js
@@ -0,0 +1,9 @@
+import { add, sub, mult, div } from './calculator';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sub(2, 2)).toBe(0);
+  });
