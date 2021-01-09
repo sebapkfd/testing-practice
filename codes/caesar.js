a8080b5e5c8a9ca1e1a018e8e5d5f00d6decc315
Seba Soto <sebastian.pkfd@gmail.com>
2021-01-08 21:30:11 -0300

    Caesar current tests solved

--- codes/caesar.js
+++ codes/caesar.js
@@ -1,5 +1,52 @@
+const getKeyByValue = (object, value) => {
+    return Object.keys(object).find(key => object[key] === value);
+}
+
+const isUpperCase = (str) => {
+    return str === str.toUpperCase();
+}
+
 const caesar = (str, shift) => {
-    return 'xd';
+    const code = {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: 4,
+        e: 5,
+        f: 6,
+        g: 7,
+        h: 8,
+        i: 9,
+        j: 10,
+        k: 11,
+        l: 12,
+        m: 13,
+        n: 14,
+        o: 15,
+        p: 16,
+        q: 17,
+        r: 18,
+        s: 19,
+        t: 20,
+        u: 21,
+        v: 22,
+        w: 23,
+        x: 24,
+        y: 25,
+        z: 26
+    }
+
+    let aux = str.split('');
+    let newArray = aux.map((a) => {
+        if(a in code || a.toLowerCase() in code){
+            let newValue = (isUpperCase(a)) ? (code[a.toLowerCase()] + shift)%26 : (code[a] + shift)%26;
+            let key = getKeyByValue(code, newValue);
+            return (isUpperCase(a)) ? key.toUpperCase(): key;
+        }
+        return a;
+    })
+    return newArray.join('');
+
 }
 
 export default caesar;
--- tests/caesar.test.js
+++ tests/caesar.test.js
@@ -1,14 +1,13 @@
-import { TestScheduler } from 'jest';
 import caesar from '../codes/caesar';
 
 test('abcdefgh to bcdefghi', () => {
     expect(caesar('abcdefgh', 1)).toBe('bcdefghi')
 })
 
-test('Sebastian to colkcdskx', () => {
+test('Sebastian to Colkcdskx', () => {
     expect(caesar('Sebastian', 10)).toBe('Colkcdskx')
 })
 
 test('Third.test to Drsbn.docd', () => {
-    expect(caesar('Third.test', 1)).toBe('Drsbn.docd')
+    expect(caesar('Third.test', 10)).toBe('Drsbn.docd')
 })
